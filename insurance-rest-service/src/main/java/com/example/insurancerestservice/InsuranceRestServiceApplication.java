6c7c164578d3694f496ec52df7186b6f0e179e2a
Harsh K <hersh.k17@gmail.com>
2023-07-12 02:27:39 -0400

    initial commit

---
+++ insurance-rest-service/src/main/java/com/example/insurancerestservice/InsuranceRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceRestServiceApplication.class, args);
+    }
+
+}
